183bea29c89eda92cfed1e3ece9c31bd92919c89
demiurgZorge <andrey777zhuravl@gmail.com>
2018-12-29 09:49:53 +0300

    fix ResponseBody

--- src/main/java/com/microservices/user/web/LoginController.java
+++ src/main/java/com/microservices/user/web/LoginController.java
@@ -45,6 +45,7 @@ public class LoginController {
      * @return RetValue<LoginResponseModel>
      */
     @RequestMapping(value="/loginWithToken", method=RequestMethod.POST, produces = "application/json")
+    @ResponseBody
     public ApiSuccessResult<UserDto> loginWithToken(HttpServletRequest request, @RequestBody String token) {        
         return ApiResult.success(userLogic.loginWithToken(token, headersReader.getHttpHeaders(request)));
     }
