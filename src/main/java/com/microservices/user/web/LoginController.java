77575f17a00cbdeb0cf1e8a11b34db46d3860ae9
demiurgZorge <andrey777zhuravl@gmail.com>
2018-12-29 12:28:15 +0300

    import

--- src/main/java/com/microservices/user/web/LoginController.java
+++ src/main/java/com/microservices/user/web/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.microservices.user.core.apiresult.ApiResult;
 import com.microservices.user.core.apiresult.ApiSuccessResult;
